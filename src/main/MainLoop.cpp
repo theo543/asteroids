825ba639478a0933808aa2296f5f204be02d37c1
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-04-21 12:29:42 +0300

    Enable AA

--- src/main/MainLoop.cpp
+++ src/main/MainLoop.cpp
@@ -5,7 +5,7 @@
 
 [[maybe_unused]] MainLoop::MainLoop(std::unique_ptr<WorldInterface> firstScene) : window(), world(std::move(firstScene)), realTime(), worldTime(), returnToMainLoop(false), nextScene(nullptr) {
     // NOTE: sync with env variable APP_WINDOW from .GitHub/workflows/cmake.yml:30
-    window.create(sf::VideoMode({800, 700}), "", sf::Style::Titlebar | sf::Style::Close);
+    window.create(sf::VideoMode({800, 700}), "", sf::Style::Titlebar | sf::Style::Close, sf::ContextSettings(0, 0, 8));
     window.setVerticalSyncEnabled(true);
     window.setActive(false);
     //window.setFramerateLimit(60);
