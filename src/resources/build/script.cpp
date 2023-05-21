32d74ab8990b53418c4a1e0f3fe7a63e2671322e
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-05-21 15:52:14 +0300

    Use raw string

--- src/resources/build/script.cpp
+++ src/resources/build/script.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 
 ///TODO placeholder!
-constexpr auto placeholder =
-        "#include \"resources/EmbeddedAccessor.h\"\n"
-        "typedef EmbeddedAccessor RB;\n"
-        "const RB::EmbeddedMap placeholder = {};"
-        "const RB::EmbeddedMap &RB::getEmbeddedResources() {\n"
-        "    return placeholder;\n"
-        "}\n";
+constexpr auto placeholder = R"(
+    #include "resources/EmbeddedAccessor.h"
+    typedef EmbeddedAccessor RB;
+    const RB::EmbeddedMap placeholder = {};
+    const RB::EmbeddedMap &RB::getEmbeddedResources() {
+        return placeholder;
+    }
+)";
 
 int main(int argc, char **argv) {
     if(argc < 2) return 1;
